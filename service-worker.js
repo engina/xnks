1a043eaaca192cb9f248a07af019fabbe4365a39
engina <engin@bzzzt.biz>
2019-09-17 22:21:35 +0300

    publish

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c682a31d767952585025ffd300f93bc8.js"
+  "./precache-manifest.88116b1724dcbd3a55730a257ac4dedd.js"
 );
 
 workbox.clientsClaim();
